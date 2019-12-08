36ae29d6110302bcf304b525944c389fe02e244d
ほりちゃん <manami.eikaiwa1b@gmail.com>
2019-12-08 19:44:14 +0900

    switch文練習-16

--- let/main.js
+++ let/main.js
@@ -1,21 +1,13 @@
-function myFunction(a,b){
-    if(a >= 100){
-        return a;
-    }
+const myFruit = "メロン";
 
-    return b;
+switch(myFruit){
+    case 'りんご':
+    alert('りんごです');
+    break;
+    case 'みかん':
+    alert('みかんです');
+    break;
+    default:
+    alert('その他です');
+    break;
 }
-
-function calcFunction(price, tax){
-    const result = price + price * tax;
-    return result;
-}
-
-const myResult = calcFunction(100, 0.1);
-console.log(myResult);
-
-function myFunction(){
-    console.log('こんにちは');
-}
-
-myFunction();
