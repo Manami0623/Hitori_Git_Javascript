0205f34c8c97934ed3d2850ae9ef8b940e8f8a0b
ほりちゃん <manami.eikaiwa1b@gmail.com>
2020-03-21 12:19:40 +0900

    新しいファイル追加
    追加

---
+++ C2/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.result1').innerHTML = Math.round(6.24);
+document.querySelector('.result2').innerHTML = Math.round(6.24);
+document.querySelector('.result3').innerHTML = Math.round(6.24);
+document.querySelector('.result4').innerHTML = Math.round(6.24);
+document.querySelector('.result5').innerHTML = Math.round(-7.49);
+document.querySelector('.result6').innerHTML = Math.round(-7.49);
+document.querySelector('.result7').innerHTML = Math.round(-7.49);
+document.querySelector('.result8').innerHTML = Math.round(-7.49);
+
--- let/main.js
+++
@@ -1,8 +0,0 @@
-for(let index = 0; index < 10;index++){
-    if(index % 2 == 0){
-        continue;
-    }
-    console.log(index);
-}
-
-console.log('ループ終了');
